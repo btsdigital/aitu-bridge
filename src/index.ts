0113667385138527e8272fdca9bad085909d8c6b
Gordey Levchenko <gordey.levchenko@btsdigital.kz>
2020-07-07 10:01:35 +0300

    Promisify inovoke method, change data format

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,54 @@
+import promisifyInvoke from './promisifyInvoke';
+
 type AituInvoke = <T>(method: T, data: any) => void;
 
 interface AituBridge {
   invoke: AituInvoke;
+  sub: any;
 }
+
+const invokeMethod = 'invoke';
 
 const android = typeof window !== 'undefined' && (window as any).AndroidBridge;
 const ios = typeof window !== 'undefined' && (window as any).IosBridge;
 
-const bridge: AituBridge = {
-  invoke(method, data = {}) {
-    data.userId = '123-345-123';
-    data.testBoolean = true;
+const buildBridge = (): AituBridge => {
+  const subs = [];
 
-    if (android && android[method]) {
-      android[method](JSON.stringify(data));
+  if (window) {
+    window.addEventListener('aituEvents', (e: any) => {
+      const dataStr = JSON.stringify(e.detail);
+      console.log('subs_data', dataStr);
+      [...subs].map((fn) => fn.call(null, e));
+    })
+  }
+
+  const invoke = (req1, method, data = {}) => {
+    if (android && android[invokeMethod]) {
+      android[invokeMethod](req1, method, JSON.stringify(data));
     }
 
-    if (ios && ios[method]) {
-      ios[method].postMessage(data);
+    if (ios && ios[invokeMethod]) {
+      ios[invokeMethod].postMessage(req1, method, data);
     }
 
     console.log('=----------------------=');
     console.log('invoke method: ', method);
     console.log('params: ', data);
+  };
+
+  const sub = (listener: any) => {
+    subs.push(listener);
+  }
+
+  const invokePromise = promisifyInvoke(invoke, sub);
+
+  return {
+    invoke: invokePromise,
+    sub
   }
 }
+
+const bridge = buildBridge();
 
 export default bridge;
---
+++ src/promisifyInvoke.ts
@@ -0,0 +1,68 @@
+function createCounter() {
+  return {
+    current: 0,
+    next() {
+      return ++this.current;
+    },
+  };
+}
+
+function createRequestResolver() {
+  type PromiseController = {
+    resolve: (value: any) => any;
+    reject: (reason: any) => any;
+  };
+
+  const counter = createCounter();
+  const promiseControllers: Record<string, PromiseController | null> = {};
+
+  return {
+    add(controller: PromiseController, customId?: number | string): number | string {
+      const id = customId != null ? customId : counter.next();
+      promiseControllers[id] = controller;
+      return id;
+    },
+
+    resolve<T>(reqId: number | string, data: T, isSuccess: (data: T) => boolean) {
+      const requestPromise = promiseControllers[reqId];
+
+      if (requestPromise) {
+        if (isSuccess(data)) {
+          requestPromise.resolve(data);
+        } else {
+          requestPromise.reject(data);
+        }
+
+        promiseControllers[reqId] = null;
+      }
+    },
+  };
+}
+
+function promisifyInvoke(invoke, subscribe: (fn: any) => void) {
+  const requestResolver = createRequestResolver();
+
+  subscribe((event) => {
+    if (!event.detail || !event.detail.data) {
+      return;
+    }
+
+    if ('reqId' in event.detail) {
+      const { reqId, data } = event.detail;
+
+      if (reqId) {
+        requestResolver.resolve(reqId, data, (data) => !('error' in event.detail));
+      }
+    }
+  });
+
+  return function promisifiedSend(method: any, props): Promise<any | void> {
+    return new Promise((resolve, reject) => {
+      const reqId = requestResolver.add({ resolve, reject }, props.reqId);
+
+      invoke(reqId, method, props);
+    });
+  };
+}
+
+export default promisifyInvoke;

37111e55dedb8bb2a2b655075e480382bc8f7944
Gordey Levchenko <levchenko.gordey@gmail.com>
2020-07-09 10:49:25 +0300

    Remove console.logs

--- src/index.ts
+++ src/index.ts
@@ -17,27 +17,18 @@ const buildBridge = (): AituBridge => {
 
   if (typeof window !== 'undefined') {
     window.addEventListener('aituEvents', (e: any) => {
-      const dataStr = JSON.stringify(e.detail);
-      console.log('subs_data', dataStr);
       [...subs].map((fn) => fn.call(null, e));
     })
   }
 
   const invoke = (reqId, method, data = {}) => {
-    console.log('_start_____INVOKE');
-    console.log('reqId: ', reqId);
-    console.log('method: ', method);
-    console.log('data: ', data);
     if (android && android[invokeMethod]) {
       android[invokeMethod](reqId, method, JSON.stringify(data));
     }
 
-    console.log('----before ios PostMessage');
     if (ios && ios[invokeMethod]) {
-      console.log('----INSIDE if - GO ios PostMessage');
       ios[invokeMethod].postMessage({ reqId, method, data });
     }
-    console.log('----after ios PostMessage');
   };
 
   const sub = (listener: any) => {

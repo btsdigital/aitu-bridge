400cb6f958e762a003ff1ac3b4eaa77d555f3ae5
Gordey Levchenko <levchenko.gordey@gmail.com>
2020-09-14 13:34:10 +0300

    Add geo and settings method

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import promisifyInvoke from './promisifyInvoke';
 import promisifyStorage from './promisifyStorage';
+import promisifyMethod from './promisifyMethod';
 
 type InvokeRequest = 'GetMe' | 'GetPhone' | 'GetContacts';
 
@@ -14,7 +15,12 @@ interface ResponseObject {
   lastname?: string;
 }
 
+// interface GetGeoResponse {}
+// interface OpenSettingsResponse {}
+
 type BridgeInvoke<T extends InvokeRequest> = (method: T, data?: {}) => Promise<ResponseObject>;
+type BridgeGetGeo = () => Promise<any>;
+type BridgeOpenSettings = () => Promise<any>;
 
 interface BridgeStorage {
   setItem: SetItemType,
@@ -24,12 +30,16 @@ interface BridgeStorage {
 interface AituBridge {
   invoke: BridgeInvoke<InvokeRequest>;
   storage: BridgeStorage;
+  getGeo: BridgeGetGeo;
+  openSettings: BridgeOpenSettings;
   isSupported: () => boolean;
   sub: any;
 }
 
 const invokeMethod = 'invoke';
 const storageMethod = 'storage';
+const getGeoMethod = 'getGeo';
+const openSettingsMethod = 'openSettings';
 
 const android = typeof window !== 'undefined' && (window as any).AndroidBridge;
 const ios = typeof window !== 'undefined' && (window as any).webkit && (window as any).webkit.messageHandlers;
@@ -69,6 +79,32 @@ const buildBridge = (): AituBridge => {
     }
   }
 
+  const getGeo = (reqId, data = {}) => {
+    const isAndroid = android && android[getGeoMethod];
+    const isIos = ios && ios[getGeoMethod];
+
+    if (isAndroid) {
+      android[getGeoMethod](reqId, JSON.stringify(data));
+    } else if (isIos) {
+      ios[getGeoMethod].postMessage({ reqId, data });
+    } else if (typeof window !== 'undefined') {
+      console.log('--getGeo-isWeb');
+    }
+  }
+
+  const openSettings = (reqId, method, data = {}) => {
+    const isAndroid = android && android[openSettingsMethod];
+    const isIos = ios && ios[openSettingsMethod];
+
+    if (isAndroid) {
+      android[openSettingsMethod](reqId, method, JSON.stringify(data));
+    } else if (isIos) {
+      ios[openSettingsMethod].postMessage({ reqId, method, data });
+    } else if (typeof window !== 'undefined') {
+      console.log('--openSettings-isWeb');
+    }
+  }
+
   const isSupported = () => {
     return android || ios;
   }
@@ -79,10 +115,14 @@ const buildBridge = (): AituBridge => {
 
   const invokePromise = promisifyInvoke(invoke, sub);
   const storagePromise = promisifyStorage(storage, sub);
+  const getGeoPromise = promisifyMethod(getGeo, sub);
+  const openSettingsPromise = promisifyMethod(openSettings, sub);
 
   return {
     invoke: invokePromise,
     storage: storagePromise,
+    getGeo: getGeoPromise,
+    openSettings: openSettingsPromise,
     isSupported,
     sub
   }

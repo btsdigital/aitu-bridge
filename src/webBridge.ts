0d896b464d54df3822ade0dc17e5dc6cd5faeaa0
Nurbek <nurbek.zhussip@btsdigital.kz>
2022-12-09 16:22:48 +0600

    fix: update aitu origin

---
+++ src/webBridge.ts
@@ -0,0 +1,44 @@
+import type { AituBridge } from './index';
+
+const AITU_DOMAIN_PARAM = '__aitu-domain'
+
+const searchParams = new URLSearchParams(window.location.search)
+
+let aituOrigin = searchParams.get(AITU_DOMAIN_PARAM)
+
+if(aituOrigin){
+    localStorage.setItem('mini-app-domain', aituOrigin)
+}else{
+    aituOrigin = localStorage.getItem('mini-app-domain')
+}
+interface WebBridge {
+    execute(method: keyof AituBridge, reqId: string, ...payload: any[] ): void
+    origin: string
+}
+
+let WebBridge: WebBridge | null = null
+
+if (aituOrigin) {
+    WebBridge = {
+        origin: aituOrigin,
+        execute: (method, reqId, ...payload) => {
+            window.top.postMessage({
+                    source: 'aitu-bridge',
+                    method,
+                    reqId,
+                    payload: [...payload],
+                },
+                WebBridge.origin
+            )
+    }
+
+    }
+    
+    window.addEventListener('message', event => {
+        if (event.origin === aituOrigin && event.data) {
+            window.dispatchEvent(new CustomEvent('aituEvents', { detail: event.data }));
+        }
+    })
+}
+
+export default WebBridge

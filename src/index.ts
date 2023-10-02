26d0e09ef7e2b197a5b7d094bfa0d8a1c44f3e93
Nurbek <nurbek.zhussip@btsdigital.kz>
2023-10-02 15:28:27 +0600

    AITU-21796 refactor: update getMeResponse

--- src/index.ts
+++ src/index.ts
@@ -37,6 +37,8 @@ interface GetMeResponse {
   id: string;
   avatar?: string;
   avatarThumb?: string;
+  notifications_allowed: boolean;
+  private_messaging_enabled: boolean;
   sign: string;
 }
 
@@ -187,7 +189,7 @@ const buildBridge = (): AituBridge => {
     })
 
     window.addEventListener('message', (e) => {
-      if(typeof e.data !== 'string') {
+      if (typeof e.data !== 'string') {
         return;
       }
 

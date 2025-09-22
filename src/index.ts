e02ca944b1c3e691dbf62136cbd5ad47cbb431f0
Sergey Gordienko <sergei.gordienko@btsdigital.kz>
2025-09-22 19:36:26 +0300

    AITU-26336 Fix including error.d.ts

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import {
 } from './utils'
 
 import WebBridge from './webBridge';
+export * from './error';
 
 enum EInvokeRequest {
   getMe = 'GetMe',
--- src/version.ts
+++ src/version.ts
@@ -1,1 +1,1 @@
-export const LIB_VERSION = "0.3.59";
+export const LIB_VERSION = "0.3.60";
